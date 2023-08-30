edc054c1582cb48d27430f61c3e23a311c82fbe7
GrieveDesu <143489880+GrieveDesu@users.noreply.github.com>
2023-08-30 15:31:43 +0800

    Add files via upload

---
+++ file.py
@@ -0,0 +1,24 @@
+#BotProperties
+name = "myCrushBot_Deo"
+age=16
+hobby="music"
+pet='dog'
+petName="deog"
+hairType="curly"
+sexuality="gay"
+nickname="Deo"
+sex="male"
+reply="yes"
+print("d:HELLO PLAYER, WHAT IS YOUR name?")
+uName = str(input())
+print("d:Good morning, ", uName,", I'm ",name)
+print("y:H-How did you know my name...?! *blushes*")
+print("d:I like",hobby)
+print("d:...and um, maybe y-you as well..." )
+print("y:w-what?!")
+print("y:(his hair is so",hairType," i love it!!!)")
+print("d:i'm",sexuality)
+print("y:omg me too!")
+print("y:c-c-c-can i call you",nickname)
+print("d:",reply,", I'm a",sex,"btw")
+print("y:omg me too!")
